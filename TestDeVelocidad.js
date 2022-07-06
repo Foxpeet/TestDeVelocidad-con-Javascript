6a7bf4191304896ef806555b42c103af3138452c
Foxpeet <diegobal007@gmail.com>
2022-07-06 12:17:30 +0200

    Calculo del tiempo empleado

--- TestDeVelocidad.js
+++ TestDeVelocidad.js
@@ -1,15 +1,24 @@
-var imageAddr = "http://www.tranquilmusic.ca/images/cats/Cat2.JPG" + "?n=" + Math.random();
 var startTime, endTime;
 var downloadSize = 5616998;
 var download = new Image();
-download.onload = function () {
-    endTime = (new Date()).getTime();
-    showResults();
-}
-startTime = (new Date()).getTime();
-download.src = imageAddr;
 
-function showResults() {
+function EjecutarPrueba(){
+    var imageAddr = "http://www.tranquilmusic.ca/images/cats/Cat2.JPG" + "?n=" + Math.random();
+    download.src = imageAddr;
+    console.log("He comenzado")
+    startTime = (new Date()).getTime();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        mostrarResultados();
+    }
+}
+
+function ajustarTiempo(time){
+    let segundos = time/1
+    return (segundos + " sec")
+}
+
+function mostrarResultados() {
     var duration = (endTime - startTime) / 1000; //Math.round()
     var bitsLoaded = downloadSize * 8;
     var speedBps = (bitsLoaded / duration).toFixed(2);
@@ -20,4 +29,5 @@ function showResults() {
         speedKbps + " kbps\n" +
         speedMbps + " Mbps\n"
     );
+    console.log(ajustarTiempo(duration))
 }
